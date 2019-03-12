fb9409f0ffdcc191dd5c136cbf3699a692036677
bhakti-ghatnekar1050 <bhaktighatnekar@gmail.com>
2019-03-12 14:24:33 +0530

    Update ApiClient.js

--- javascript-client/src/ApiClient.js
+++ javascript-client/src/ApiClient.js
@@ -29,7 +29,6 @@
    * @module ApiClient
    * @version 1.0.0
    */
-
   /**
    * Manages low level client-server communications, parameter marshalling, etc. There should not be any need for an
    * application to use this class directly - the *Api and model classes provide the public API for the service. The
